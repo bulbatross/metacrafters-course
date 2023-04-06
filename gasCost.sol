cc7bb1fc4f1d2e6aa5847ed0bb00ac06412556f2
bulbatross <johanejdemark@hotmail.com>
2023-04-06 22:52:20 +0200

    Create gasCost.sol
    A showcase of how the gas cost differ depending on a variable is set as constant or not

---
+++ gasCost.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract gasEater{
+    uint public constant SOME_FUNNY_NUMBER = 582;
+}
+
+contract biggerGasEater{
+    uint public aMoreExpensiveNumber = 582;
+}
