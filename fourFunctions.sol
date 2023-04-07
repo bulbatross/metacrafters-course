ac5d87dc901878b0c0f06bb7f65cbe1d84e99026
bulbatross <johanejdemark@hotmail.com>
2023-04-07 23:03:04 +0200

    Create fourFunctions.sol

---
+++ fourFunctions.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.7;
+
+contract fourFunctions{
+    function addition(uint a, uint b) external pure returns(uint){
+        return a + b;
+    }
+    function substraction(uint a, uint b) external pure returns(uint){
+        return a - b;
+    }
+    function multiplication(uint a, uint b) external pure returns(uint){
+        return a*b;
+    }
+    function division(uint a, uint b) external pure returns(uint){
+        return a/b;
+    }
+}
